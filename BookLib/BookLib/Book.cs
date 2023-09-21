04d1ff9db5310a8a8c174959a7ba4dfb2db9a70c
nhthomsen <nooblas@hotmail.com>
2023-09-21 13:26:57 +0200

    Added book class and validation

---
+++ BookLib/BookLib/Book.cs
@@ -0,0 +1,41 @@
+﻿namespace BookLib
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Price { get; set; }
+
+        private void ValidateTitle()
+        {
+            if(Title == null)
+            {
+                throw new ArgumentNullException("title is null");
+            }
+            if(Title.Length < 3)
+            {
+                throw new ArgumentOutOfRangeException("title must be 3 characters or longer");
+            }
+        }
+        private void ValidatePrice() 
+        {
+            if (Price < 0)
+            {
+                throw new ArgumentOutOfRangeException("Price must be greater than 0");
+            }
+            if(Price > 1201) 
+            {
+                throw new ArgumentOutOfRangeException("Price must be less than or equal 1200");
+            }
+        }
+        public void Validate() 
+        {
+            ValidateTitle();
+            ValidatePrice();
+        }
+        public override string ToString()
+        {
+            return "Id: " + Id + " Title: " + Title + " Price: " + Price; 
+        }
+    }
+}
--- BookLib/BookLib/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace BookLib
-{
-    public class Class1
-    {
-
-    }
-}
