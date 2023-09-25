e1296185eab1c73ff8599cf7dd7ae7ef358d11e4
nhthomsen <nooblas@hotmail.com>
2023-09-25 09:37:54 +0200

    Added functionality

--- BookLib/BookLib/BooksRepository.cs
+++ BookLib/BookLib/BooksRepository.cs
@@ -19,11 +19,71 @@ namespace BookLib
         public List<Book> Get(int? priceLessThan = null, int? priceGreaterThan = null, string? sortBy = null)
         {
             List<Book> bookList = new List<Book>(books);
+            if(priceLessThan != null)
+            {
+                bookList.Where(b => b.Price < priceLessThan);
+            }
+            if(priceGreaterThan != null)
+            {
+                bookList.Where(b => b.Price > priceGreaterThan);
+            }
+            if(sortBy != null)
+            {
+                switch(sortBy.ToLower())
+                {
+                    case "title":
+                    case "title_asc":
+                        bookList.OrderBy(b => b.Title);
+                        break;
+                    case "title_desc":
+                        bookList.OrderByDescending(b => b.Title);
+                        break;
+                    case "price":
+                    case "price_asc":
+                        bookList.OrderBy(b => b.Price);
+                        break;
+                    case "price_desc":
+                        bookList.OrderByDescending(b => b.Price);
+                        break;
+                    default:
+                        break;
+                }
+            }
             return bookList;
         }
         public Book? GetById(int id)
         {
             return books.FirstOrDefault(book =>  book.Id == id);
+        }
+        public Book Add(Book book)
+        {
+            book.Validate();
+            int currentMaxId = books.Select(b => b.Id).Max();
+            book.Id = currentMaxId++;
+            books.Add(book);
+            return book;
+        }
+        public Book? Delete(int id) 
+        {
+            Book? bookToDelete = GetById(id);
+            if(bookToDelete == null)
+            {
+                return null;
+            }
+            books.Remove(bookToDelete);
+            return bookToDelete;
+        }
+        public Book? Update(int id, Book values)
+        {
+            values.Validate();
+            Book? bookToUpdate = GetById(id);
+            if (bookToUpdate == null)
+            {
+                return null;
+            }
+            bookToUpdate.Title = values.Title;
+            bookToUpdate.Price = values.Price;
+            return bookToUpdate;
         }
     }
 }
